2537eb9f01d5b6cfa263fe05c82a59a39a449c24
dramos89 <corey.lacrimosa@gmail.com>
2016-09-24 23:58:12 -0300

    Comentario da classe DiaDaSemanaNumerico.java para não gerar erro

--- Aula2/src/main/java/aula2/DiaDaSemanaNumerico.java
+++ Aula2/src/main/java/aula2/DiaDaSemanaNumerico.java
@@ -1,15 +1,16 @@
 package aula2;
 
-import java.util.Scanner;
+
 
 public class DiaDaSemanaNumerico {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		/*
+		 * Scanner input = new Scanner(System.in);
 		String diaEx = input.nextLine();
 		int dia = 0;
 		switch(diaEx.toUpperCase()){
 		case "Segunda":
 			dia = 0;
-		}
+		}*/
 	}
 }
