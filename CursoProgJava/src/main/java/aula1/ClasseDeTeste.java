68cd09251776e07b4740745d065b06757fe04eb7
dramos89 <corey.lacrimosa@gmail.com>
2016-09-25 09:30:49 -0300

    25/09/2016: Commit para transformar as aulas em pacotes e agrupar todas as aulas (e respectivas classes) em um único projeto de nome CursoProgJava

--- CursoProgJava/src/main/java/aula1/ClasseDeTeste.java
+++ CursoProgJava/src/main/java/aula1/ClasseDeTeste.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 
 public class ClasseDeTeste {
 	public static void main(String[] args) {
-		System.out.println(StringUtils.center("bola boleta bolado",15));
+		System.out.println(StringUtils.abbreviate("Diego Ramos da Silva",10));
 	}
 }
