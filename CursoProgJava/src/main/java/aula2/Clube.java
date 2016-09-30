bb7ca849acc1ffd6d5faab537002a7def0d0e7fd
dramos89 <corey.lacrimosa@gmail.com>
2016-09-30 19:57:02 -0300

    30/09/2016: 0 - Commit das classes prototipo para otimizar o exercicio de processar resultados de confrontos de times de futebol.

--- CursoProgJava/src/main/java/aula2/Clube.java
+++
@@ -1,7 +0,0 @@
-package aula2;
-
-public class Clube {
-	String nome;
-	Partidas[] placar;
-	
-}
---
+++ CursoProgJava/src/main/java/aula2/Confrontos.java
@@ -0,0 +1,46 @@
+package aula2;
+
+public class Confrontos {
+	TimeDeFutebol[] times;
+	int qtdConfrontos;
+	int qtdClubes;
+	
+	Confrontos(String[] nomesTimes, int qtdConfrontos, int qtdClubes){
+		this.qtdConfrontos = qtdConfrontos;
+		this.qtdClubes = qtdClubes;
+		for (int x = 0; x < this.qtdClubes; x++){
+			this.times[x] = new TimeDeFutebol(nomesTimes[x]);
+		}
+	}
+	
+	void setPlacar(int[][] placar){
+		for (int x = 0; x < this.qtdConfrontos; x++){
+			for (int y = 0; y < this.qtdClubes; y++){
+				this.times[y].setPlacar(placar[x][y], x);
+			}
+		}
+	}
+	
+	void geraSaldo(){
+		for (int x = 0; x < this.qtdClubes; x++){
+			this.times[x].geraGolsPro();
+		}
+		//faz processamento cruzado pra receber e passar o valor no calculo de saldo
+		for (int x = 0; x < this.qtdClubes; x++){
+			for (int y = this.qtdClubes; y > 0; y--){
+				if (!(this.times[x].equals(this.times[y].nome))){
+					this.times[x].processaSaldo(this.times[y].golsPro);
+				}
+			}
+		}
+	}
+	//PENSAR NA LOGICA PRA PROCESSAR QUANDO É MAIS DE DOIS TIMES, PRA MANTER GENERICO
+	void processaPlacares(){
+		for (int x = 0; x < this.qtdConfrontos; x++){
+			for (int y = 0; y < this.qtdClubes; y++){
+				
+			}
+		}
+	}
+	
+}
